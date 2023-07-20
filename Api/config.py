2e8b8d79e814ff4beb9597c4dbd5cdb49254cbea
harsh0506 <joshiharsh0506@gmail.com>
2023-07-20 18:57:26 +0530

    statrted database integration

--- Api/config.py
+++ Api/config.py
@@ -1,4 +1,4 @@
 class Config:
     # Replace 'your_database_uri' with the connection URI to your PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = 'postgresql://default:IXqtsuO3gFP6@ep-empty-wave-816775.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb?options=endpoint%3Dep-empty-wave-816775'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
